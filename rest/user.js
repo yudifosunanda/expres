0e1a470b45e51a7b80ebb85fff1aafc57b5ccb60
Yudifo Sunanda <sunandadifo@gmail.com>
2024-07-08 11:05:50 +0700

    feat : add uploads

--- rest/user.js
+++ rest/user.js
@@ -1,28 +1,60 @@
 const db = require('./../db');
-
+const { query } = require('express-validator');
 
 const getUser = async (req, res) => {
   try {
-    const result = await db.query('SELECT * FROM "user" where "usr_id" = 1');
+    const result = await db.query('SELECT * FROM "user"');
     res.json(result.rows); // Send the rows as JSON
   } catch (err) {
     res.status(500).send('Error executing query');
   }
 };
 
-const addUser = (req, res) => {
-  // Logic to add a user
-  res.json({ message: "User added" });
+const addUser = async(req, res) => {
+  let {name, email, gender, picture} = req.body;
+  const profilePic = req.file ? req.file.filename : null;
+
+  try {
+    const result = await db.query(
+      'INSERT INTO "user" (name, email, gender, picture) VALUES ($1, $2, $3 ,$4) RETURNING *',
+      [name, email, gender, profilePic]
+    );
+    res.json(result.rows); // Send the rows as JSON
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).send('Error executing query');
+  }
 };
 
-const updateUser = (req, res) => {
-  // Logic to update a user
-  res.json({ message: "User updated" });
+const updateUser = async(req, res) => {
+  const { id } = req.params; // Extract id from URL parameters
+  let {name, email, gender} = req.body;
+
+  try {
+    const result = await db.query(
+      `UPDATE "user" set name = $1 , email  = $2 , gender = $3 where usr_id = $4 RETURNING *`,
+      [name, email, gender, id]
+    );
+    res.json(result.rows); // Send the rows as JSON
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).send('Error executing query');
+  }
 };
 
-const deleteUser = (req, res) => {
-  // Logic to delete a user
-  res.json({ message: "User deleted" });
+const deleteUser = async (req, res) => {
+  const { id } = req.params; // Extract id from URL parameters
+
+  try {
+    const result = await db.query(
+      `DELETE from "user" where usr_id = $1`,
+      [id]
+    );
+    res.json("success deleted user"); // Send the rows as JSON
+  } catch (err) {
+    console.error('Error executing query', err);
+    res.status(500).send('Error executing query');
+  }
 };
 
 module.exports = {
--- server.js
+++ server.js
@@ -3,7 +3,14 @@ const express = require('express');
 const db = require('./db');
 const bodyParser = require('body-parser');
 const cors = require('cors');
-const { getUser } = require('./rest/user');
+// user model
+const { getUser, addUser, updateUser, deleteUser } = require('./rest/user');
+//validator
+const { addUserValidationRules, updateUserValidationRules } = require('./rest/userValidator');
+const { validationResult } = require('express-validator');
+//file inputs
+const multer = require('multer');
+const path = require('path');
 
 // Initialize Express app
 const app = express();
@@ -13,36 +20,77 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+
+// Configure multer for file uploads
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, 'uploads/'); // Set the destination folder for uploaded files
+  },
+  filename: function (req, file, cb) {
+    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+    cb(null, file.fieldname + '-' + uniqueSuffix + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  fileFilter: function (req, file, cb) {
+    const filetypes = /jpeg|jpg|png/; // Allowed file types
+    const mimetype = filetypes.test(file.mimetype);
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+
+    return cb(null, true);
+   
+  }
+});
+
+// Middleware to handle validation errors
+const validate = (req, res, next) => {
+  const errors = validationResult(req);
+  if (errors.isEmpty()) {
+    return next();
+  }
+  const extractedErrors = errors.array().map(err => ({ [err.path]: err.msg }));
+  return res.status(422).json({
+    errors: extractedErrors,
+  });
+};
+
 // Define a route to fetch data from the user table
-app.get('/users', getUser);
 
 app.get('/home', (req, res)=>{
   let datas = [
     {
-    "name":"difo",
-    "gender":"male"
+      "name":"difo",
+      "gender":"male"
     }
     ,{
-    "name":"difo",
-    "gender":"male"
+      "name":"difo",
+      "gender":"male"
     }
   ];
-    res.json(datas);
+  res.json(datas);
 });
 
 app.get('/', (req, res)=>{
   let datas = [
     {
-    "name":"difo",
-    "gender":"male"
+      "name":"difo",
+      "gender":"male"
     }
     ,{
-    "name":"difo",
-    "gender":"male"
+      "name":"difo",
+      "gender":"male"
     }
   ];
-    res.json(datas);
+  res.json(datas);
 });
+
+  app.get('/users', getUser);
+  app.post('/user/add', upload.single('profilePic'), addUserValidationRules(), validate, addUser);
+  app.put('/user/update/:id', updateUser);
+  app.delete('/user/delete/:id', deleteUser);
+
 
 // Start the server
 app.listen(3000, () => {
